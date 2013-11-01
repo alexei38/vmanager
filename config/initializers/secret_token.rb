12564ec70c096b282e7bc87c2788b6d53ee5b426
Alexei Margasov <amargasov@naumen.ru>
2013-11-01 22:04:43 +0600

    temporary comment gem libvirt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vmanager::Application.config.secret_token = '0bce6b4a00c91bfd5a3615b2e00850ea6615aec0e03e61487bba85d1f8b38efcc951a3db895b0ab9dcc08deba09a439ffb5e2147f101dfb73e8f75cf8e865992'
